0839b8baebabe8b801c33725f7110c343d6705a4
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-23 16:04:49 -0400

    feature: add counter and handlers to increment or decrement.

--- src/components/KeyboardControlledCounter.tsx
+++ src/components/KeyboardControlledCounter.tsx
@@ -1,21 +1,36 @@
+import { useState } from "react";
 
-const KeyBoardControlledCounter: React.FC = () => {
+const KeyboardControlledCounter: React.FC = () => {
+  const [count, setCount] = useState<number>(0)
+
+  // Handlers
+  const handleIncrement = () => {
+    setCount(prev => prev + 1);
+  }
+
+  const handleDrecrement = () => {
+    setCount(prev => (prev > 0 ? prev - 1 : 0));
+  }
   
   return (
-    <section className="container">
+    <section className="container my-3 text-center">
       <h2>Keyboard Controlled Counter</h2>
+      <div className="d-flex justify-content-center">
+        <div className="card text-center" style={{ width: "18rem"}}>
+          <div className="card-body">
+            <h3 className="card-title">{count}</h3>
 
-      <div className="card text-center" style={{ width: "18rem"}}>
-        <div className="card-body">
-          <h3 className="card-title">Count</h3>
-
-          <button className="btn btn-succes m-1">Increment</button>
-          <button className="btn btn-warning m-1">Decrement</button>
-        </div>
-
+            <button className="btn btn-succes m-1"
+            onClick={handleIncrement}
+            >Increment</button>
+            <button className="btn btn-warning m-1"
+            onClick={handleDrecrement}
+            >Decrement</button>
+          </div>
+          </div>
       </div>
     </section>
   )
 }
 
-export default KeyBoardControlledCounter;
+export default KeyboardControlledCounter;
